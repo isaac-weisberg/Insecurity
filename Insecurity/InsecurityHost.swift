7bd23a6db147902a303b6b0ad594807e22c10137
Isaac Weisberg <net.caroline.weisberg@gmail.com>
2021-11-01 02:00:43 +0300

    [WIP] InsecurityHost: start rounding out the navigation controller related details

--- Insecurity/InsecurityHost.swift
+++ Insecurity/InsecurityHost.swift
@@ -49,6 +49,7 @@ public class InsecurityHost {
                 weak var viewController: UIViewController?
             }
             
+            let children: [Child]
             weak var navigationController: UINavigationController?
         }
         
@@ -178,7 +179,7 @@ public class InsecurityHost {
             }
         }
         
-        dispatchModal(controller, animated, child)
+        sendOffModal(controller, animated, child)
     }
     
     private func finalizeModal(
@@ -230,7 +231,7 @@ public class InsecurityHost {
         self.state.stage = .ready
     }
     
-    private func dispatchModal(_ controller: UIViewController, _ animated: Bool, _ child: CommonModalCoordinatorAny) {
+    private func sendOffModal(_ controller: UIViewController, _ animated: Bool, _ child: CommonModalCoordinatorAny) {
         let electedHostControllerOpt: UIViewController?
         if let topFrame = frames.last {
             if let hostController = topFrame.viewController {
@@ -257,12 +258,222 @@ public class InsecurityHost {
         let frame = Frame.regular(Frame.Regular(state: .live, coordinator: child, viewController: controller))
         self.frames.append(frame)
         
-        electedHostController.present(controller, animated: true, completion: nil)
+        electedHostController.present(controller, animated: animated, completion: nil)
     }
     
     // MARK: - Navigation Current
     
+    func start<Coordinator: CommonNavigationCoordinator>(
+        _ child: Coordinator,
+        animated: Bool,
+        _ completion: @escaping (Coordinator.Result?) -> Void
+    ) {
+        guard state.notDead else { return }
+        
+        switch state.stage {
+        case .ready:
+            immediateDispatchNavigation(child, animated: animated) { result in
+                completion(result)
+            }
+        case .batching:
+            if scheduledStartRoutine != nil {
+                assertionFailure("Another child is waiting to be started; can't start multiple children at the same time")
+                return
+            }
+            
+            scheduledStartRoutine = { [weak self] in
+                guard let self = self else { return }
+                
+                self.scheduledStartRoutine = nil
+                self.immediateDispatchNavigation(child, animated: animated) { result in
+                    completion(result)
+                }
+            }
+        case .purging:
+            assertionFailure("Please don't start during purges")
+        }
+    }
     
+    func immediateDispatchNavigation<Coordinator: CommonNavigationCoordinator>(
+        _ child: Coordinator,
+        animated: Bool,
+        _ completion: @escaping (Coordinator.Result?) -> Void
+    ) {
+        guard state.notDead else { return }
+        
+        assert(state.stage == .ready)
+        
+        child._updateHostReference(self)
+        
+        weak var weakChild = child
+        weak var kvoContext: InsecurityKVOContext?
+        weak var weakController: UIViewController?
+        
+        child._finishImplementation = { [weak self] result in
+            if let kvoContext = kvoContext {
+                weakController?.insecurityKvo.removeObserver(kvoContext)
+            }
+            weakController?.deinitObservable.onDeinit = nil
+            weakChild?._finishImplementation = nil
+            
+            guard let self = self else {
+                assertionFailure("InsecurityHost wasn't properly retained. Make sure you save it somewhere before starting any children.")
+                return
+            }
+            guard let child = weakChild else { return }
+            
+            self.finalizeNavigation(child, .callback) {
+                completion(result)
+            }
+        }
+        
+        let controller = child.viewController
+        weakController = controller
+        
+        kvoContext = controller.insecurityKvo.addHandler(
+            UIViewController.self,
+            parentObservationKeypath
+        ) { [weak self, weak child] oldController, newController in
+            if let kvoContext = kvoContext {
+                weakController?.insecurityKvo.removeObserver(kvoContext)
+            }
+            weakController?.deinitObservable.onDeinit = nil
+            weakChild?._finishImplementation = nil
+            
+            if oldController != nil, newController == nil {
+                guard let self = self else {
+                    assertionFailure("InsecurityHost wasn't properly retained. Make sure you save it somewhere before starting any children.")
+                    return
+                }
+                guard let child = child else { return }
+            
+                self.finalizeNavigation(child, .kvo) {
+                    completion(nil)
+                }
+            }
+        }
+        
+        controller.deinitObservable.onDeinit = { [weak self, weak child] in
+            weakChild?._finishImplementation = nil
+            
+            guard let self = self, let child = child else { return }
+            
+            self.finalizeNavigation(child, .deinitialization) {
+                completion(nil)
+            }
+        }
+        
+        sendOffNavigation(controller, animated, child)
+    }
+    
+    private func finalizeNavigation(
+        _ child: CommonNavigationCoordinatorAny,
+        _ kind: FinalizationKind,
+        _ callback: () -> Void
+    ) {
+//        let indexOfChildOpt = frames.firstIndex(where: { frame in
+//            switch frame {
+//            case .regular(let regular):
+//                return regular.coordinator === child
+//            case .navigation:
+//                return false
+//            }
+//        })
+//
+//        if let indexOfChild = indexOfChildOpt {
+//            switch frames[indexOfChild] {
+//            case .regular(let regular):
+//                let newState: Frame.State
+//
+//                switch kind {
+//                case .callback:
+//                    newState = .finishedByCompletion
+//                case .kvo:
+//                    newState = .finishedByKVO
+//                case .deinitialization:
+//                    newState = .finishedByDeinit
+//                }
+//
+//                frames[indexOfChild] = .regular(
+//                    Frame.Regular(
+//                        state: newState,
+//                        coordinator: regular.coordinator,
+//                        viewController: regular.viewController
+//                    )
+//                )
+//            case .navigation:
+//                fatalError()
+//            }
+//        }
+//
+//        self.state.stage = .batching
+//        if self.state.notDead {
+//            callback()
+//        }
+//        self.state.stage = .purging
+//        self.purge()
+//        self.state.stage = .ready
+    }
+    
+    func sendOffNavigation(
+        _ controller: UIViewController,
+        _ animated: Bool,
+        _ child: CommonNavigationCoordinatorAny
+    ) {
+            
+    }
+    
+    // MARK: - Navigation New
+    
+    func start<Coordinator: CommonNavigationCoordinator>(
+        _ navigationController: UINavigationController,
+        _ child: Coordinator,
+        animated: Bool,
+        _ completion: @escaping (Coordinator.Result?) -> Void
+    ) {
+        
+    }
+    
+    private func sendOffNewNavigation(
+        _ navigationController: UINavigationController,
+        _ controller: UIViewController,
+        _ animated: Bool,
+        _ child: CommonNavigationCoordinatorAny
+    ) {
+        let electedHostControllerOpt: UIViewController?
+        if let topFrame = frames.last {
+            if let hostController = topFrame.viewController {
+                let hostDoesntPresentAnything = hostController.presentedViewController == nil
+                if hostDoesntPresentAnything {
+                    electedHostControllerOpt = hostController
+                } else {
+                    assertionFailure("Top controller in the modal stack is already busy presenting something else")
+                    electedHostControllerOpt = nil
+                }
+            } else {
+                assertionFailure("Top controller of modal stack is somehow dead")
+                electedHostControllerOpt = nil
+            }
+        } else {
+            electedHostControllerOpt = root.viewController
+        }
+        
+        guard let electedHostController = electedHostControllerOpt else {
+            assertionFailure("No parent was found to start a child")
+            return
+        }
+        
+        let navigatioFrameChild = Frame.Navigation.Child(state: .live,
+                                                         coordinator: child,
+                                                         viewController: controller)
+        let frame = Frame.navigation(Frame.Navigation(children: [navigatioFrameChild],
+                                                      navigationController: navigationController))
+        self.frames.append(frame)
+        
+        
+        navigationController.setViewControllers([ controller ], animated: false)
+        electedHostController.present(navigationController, animated: animated, completion: nil)
+    }
     
     // MARK: - Purge
     
